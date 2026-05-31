d5ad9402092063c7248e386a382bd78125b6aa2f
RodolfoIoLo <Zby0000001@outlook.com>
2026-05-31 15:28:03 +0800

    feat: add like controller

---
+++ internal/controller/like.go
@@ -0,0 +1,37 @@
+package controller
+
+import (
+	"BBingyan/internal/controller/param"
+	"BBingyan/internal/model"
+	"BBingyan/internal/utils"
+
+	"github.com/labstack/echo/v4"
+)
+
+func Like(c echo.Context) error {
+	var req struct {
+		PID int `query:"pid"`
+	}
+	if err := c.Bind(&req); err != nil || req.PID == 0 {
+		return param.BadRequest(c, "pid required")
+	}
+
+	if err := model.CreateLike(c.Request().Context(), utils.GetUID(c), req.PID); err != nil {
+		return param.InternalError(c, "")
+	}
+	return param.Success(c, nil)
+}
+
+func Unlike(c echo.Context) error {
+	var req struct {
+		PID int `query:"pid"`
+	}
+	if err := c.Bind(&req); err != nil || req.PID == 0 {
+		return param.BadRequest(c, "pid required")
+	}
+
+	if err := model.DeleteLike(c.Request().Context(), utils.GetUID(c), req.PID); err != nil {
+		return param.InternalError(c, "")
+	}
+	return param.Success(c, nil)
+}
